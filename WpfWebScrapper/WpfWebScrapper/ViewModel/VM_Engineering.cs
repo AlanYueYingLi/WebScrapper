1c4e992c6ef632d87cab77b7a133baae8d91151e
AlanYueYingLi <Alan.YueYingLi@gmail.com>
2022-03-01 21:46:52 +0800

    Create Engineering function based on MVVM structure

---
+++ WpfWebScrapper/WpfWebScrapper/ViewModel/VM_Engineering.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevTool.MVVM;
+
+namespace WpfWebScrapper.ViewModel
+{
+    public class VM_Engineering : ViewModelBase
+    {
+        public string ETFDataBase { get; set; }
+        public string Ticker { get; set; }
+        public string Response { get; set; }
+        public double EllapsedTime { get; set; }
+
+        public Queue<EngineeringCmdType> CmdList { get; set; }
+
+        public enum EngineeringCmdType
+        {
+            Test,
+        }
+
+        public VM_Engineering()
+        {
+            CmdList = new Queue<EngineeringCmdType>();
+        }
+    }
+}
