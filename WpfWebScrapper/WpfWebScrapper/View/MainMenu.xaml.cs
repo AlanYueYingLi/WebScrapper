0b099be4db553248154ab8b237fa48ae86d84195
AlanYueYingLi <Alan.YueYingLi@gmail.com>
2022-03-01 21:46:23 +0800

    Create MainMenu for View

---
+++ WpfWebScrapper/WpfWebScrapper/View/MainMenu.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfWebScrapper.View
+{
+    /// <summary>
+    /// MainMenu.xaml 的互動邏輯
+    /// </summary>
+    public partial class V_MainMenu : UserControl
+    {
+        public V_MainMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
