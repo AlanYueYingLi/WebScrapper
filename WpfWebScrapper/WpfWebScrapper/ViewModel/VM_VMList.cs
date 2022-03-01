d440dff37b088f938d2d790815b434a093edcffc
AlanYueYingLi <Alan.YueYingLi@gmail.com>
2022-03-01 21:47:47 +0800

    Create VMList ViewModel to store the whole application ViewModels

---
+++ WpfWebScrapper/WpfWebScrapper/ViewModel/VM_VMList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevTool.MVVM;
+
+namespace WpfWebScrapper.ViewModel
+{
+    public class VM_VMList : ViewModelBase
+    {
+        public VM_Main Main { get; set; }
+        public VM_Engineering Engineering { get; set; }
+    }
+}
